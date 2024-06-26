d6a7a241060e81408129c66bdb4d551108451965
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-26 10:38:09 -0700

    Add UI elements serializers

--- kingdomCommander/serializers.py
+++ kingdomCommander/serializers.py
@@ -17,3 +17,46 @@ class RunningApplicationSerializer(serializers.Serializer):
     automation_id = serializers.CharField(allow_null=True)
     process_id = serializers.IntegerField()
     handle = serializers.IntegerField()
+
+class RectSerializer(serializers.Serializer):
+    left = serializers.IntegerField()
+    top = serializers.IntegerField()
+    right = serializers.IntegerField()
+    bottom = serializers.IntegerField()
+
+class ImageSerializer(serializers.Serializer):
+    window_text = serializers.CharField()
+    automation_id = serializers.CharField()
+    handle = serializers.IntegerField()
+    rect = RectSerializer()
+
+class StaticTextSerializer(serializers.Serializer):
+    window_text = serializers.CharField()
+    automation_id = serializers.CharField()
+    handle = serializers.IntegerField()
+    rect = RectSerializer()
+
+class ComboBoxSerializer(serializers.Serializer):
+    window_text = serializers.CharField()
+    automation_id = serializers.CharField()
+    handle = serializers.IntegerField()
+    rect = RectSerializer()
+
+class EditFieldSerializer(serializers.Serializer):
+    window_text = serializers.CharField()
+    automation_id = serializers.CharField()
+    handle = serializers.IntegerField()
+    rect = RectSerializer()
+
+class ButtonSerializer(serializers.Serializer):
+    window_text = serializers.CharField()
+    automation_id = serializers.CharField()
+    handle = serializers.IntegerField()
+    rect = RectSerializer()
+
+class UIElementsSerializer(serializers.Serializer):
+    images = ImageSerializer(many=True)
+    static_texts = StaticTextSerializer(many=True)
+    comboboxes = ComboBoxSerializer(many=True)
+    edit_fields = EditFieldSerializer(many=True)
+    buttons = ButtonSerializer(many=True)
