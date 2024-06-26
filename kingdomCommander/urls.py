d6282ae2e1aa2940936ee03a8d09aec271083ddc
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-26 10:40:12 -0700

    Add get UI elements URL

--- kingdomCommander/urls.py
+++ kingdomCommander/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('list_running_applications/', views.list_running_applications_view, name='list_running_applications'),
     path('connect_to_application/', views.connect_to_application_view, name='connect_to_application'),
     path('find_controls/', views.find_descendants_view, name='find_controls'),
-    path('print_control_identifiers/', views.print_control_identifiers_view, name='print_control_identifiers'), #new
+    path('print_control_identifiers/', views.print_control_identifiers_view, name='print_control_identifiers'), 
+    path('get_ui_elements/', views.get_ui_elements_view, name='get_ui_elements'), #new
     path('disconnect_from_application/', views.disconnect_from_application_view, name='disconnect_from_application'),
     path('close_application/', views.close_application_view, name='close_application'), # NOT TESTED!
 ]
